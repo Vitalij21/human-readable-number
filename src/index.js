4367b09c665523a6ab79eeba19427d61ec40465f
Vitalij <1281978@gmail.com>
2022-02-24 23:46:54 +0300

    test ok - 1000 passing

--- src/index.js
+++ src/index.js
@@ -1,3 +1,53 @@
-module.exports = function toReadable (number) {
-  
-}
+module.exports = function toReadable(number) {
+    let a = {
+        0 : 'zero',
+        1: 'one',
+        2: 'two',
+        3: 'three',
+        4: 'four',
+        5: 'five',
+        6: 'six',
+        7: 'seven',
+        8: 'eight',
+        9: 'nine',
+        10: 'ten',
+        11: 'eleven',
+        12: 'twelve',
+        13: 'thirteen',
+        14: 'fourteen',
+        15: 'fifteen',
+        16: 'sixteen',
+        17: 'seventeen',
+        18: 'eighteen',
+        19: 'nineteen',
+        20: 'twenty',
+        30: 'thirty',
+        40: 'forty',
+        50: 'fifty',
+        60: 'sixty',
+        70: 'seventy',
+        80: 'eighty',
+        90: 'ninety',
+    };
+
+        if (number <= 20) {
+            return a[number]; 
+        } else if (number > 20 && number < 99 && String(number)[1] == 0) {
+            return a[Number(String(number)[0] + 0)];
+        } else if (number > 20 && number <= 99 && String(number)[1] !== 0) {
+            return a[Number(String(number)[0] + 0)] + ' ' + a[Number(String(number)[1])];
+        } else if (number > 99 && String(number)[1] == 0 && String(number)[2] == 0) {
+            return a[Number(String(number)[0])] + ' hundred';            
+        } else if (number > 99 && Number(String(number)[1] + String(number)[2]) == 19) {
+            return a[Number(String(number)[0])] + ' hundred ' + a[Number(String(number)[1] + String(number)[2])];
+        } else if (number > 99 && String(number)[1] !== 0 && String(number)[2] == 0) {
+            return a[Number(String(number)[0])] + ' hundred ' + a[Number(String(number)[1] + 0)];
+        } else if (number > 99 && String(number)[1] == 0 && String(number)[2] !== 0) {
+            return a[Number(String(number)[0])] + ' hundred ' + a[Number(String(number)[2])];
+        } else if (number > 99 && String(number)[1] !== 0 && String(number)[2] !== 0 && Number(String(number)[1] + String(number)[2]) < 19) {
+            return a[Number(String(number)[0])] + ' hundred ' + a[Number(String(number)[1] + String(number)[2])];
+        } else if (number > 99 && String(number)[1] !== 0 && String(number)[2] !== 0) {
+            return a[Number(String(number)[0])] + ' hundred ' + a[Number(String(number)[1] + 0)] + ' ' + a[Number(String(number)[2])];
+        }
+        
+};
